6854ae079b2467af6ff9aa1c3b6aca43b92d776e
xjshi <873807076@qq.com>
2019-03-21 18:27:02 +0800

    [add] 提交依赖库。

--- out.sh
+++ out.sh
@@ -1,6 +1,7 @@
 mkdir output
 cp gp_txcall ./output
-cp ./base/inifile/lib/libinifile.so ./output
+cp ./base/inifile/lib/libinifile.so  lib* ./output
 cp ./tx_dep/mysqlcnn/lib64/* ./output
 cp ./Service.ini ./output
 cp ./database.conf ./output
+
